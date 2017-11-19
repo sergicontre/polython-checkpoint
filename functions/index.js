39db6513d9c1e25bd2e74a691cf4d5ee0ee98e37
Jorge del Casar <jorge.casar@gmail.com>
2017-11-18 18:47:07 -0600

    add activeChallenge cloud function

--- functions/index.js
+++ functions/index.js
@@ -10,3 +10,22 @@ exports.createUser = functions.auth.user().onCreate(event => {
 exports.deleteUser = functions.auth.user().onDelete(event => {
   admin.database().ref('/roles/' + event.data.uid).remove();
 });
+
+exports.activeChallenge = functions.database
+	.ref('/activeChallenge')
+	.onWrite(event => {
+		const activeChallenge = event.data.val();
+		if (activeChallenge) {
+			const challengeRef = admin.database().ref('/challenges').child(activeChallenge);
+			challengeRef.child('duration').once('value').then(snapshot => {
+				// minutes to miliseconds
+				const duration = snapshot.val() * 60000;
+				const startTime = Date.now();
+				const endTime = startTime + duration;
+				challengeRef.update({
+					startTime: startTime,
+					endTime: endTime
+				})
+			});
+		}
+	});
